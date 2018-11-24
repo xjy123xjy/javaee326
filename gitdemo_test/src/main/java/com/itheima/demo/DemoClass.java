30c1639c827a1cffdd6a5a6d8853fbcb4ac53514
xjy <xjy@163.com>
2018-11-24 21:12:10 +0800

    更改

--- gitdemo_test/src/main/java/com/itheima/demo/DemoClass.java
+++ gitdemo_test/src/main/java/com/itheima/demo/DemoClass.java
@@ -4,5 +4,6 @@ public class DemoClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("提交按钮");
+        System.out.println("可以行事");
     }
 }
